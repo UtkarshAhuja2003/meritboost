819b60df93f9d35fd4c161ed2aecfddc111613fa
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-08-07 00:06:13 +0530

    minor_changes

--- src/components/home/about/index.tsx
+++ src/components/home/about/index.tsx
@@ -9,7 +9,7 @@ const About = () => {
                 <img src={img}  alt="Lakshita Ahuja" />
             </div>
             <div className="text-justify font-normal md:tracking-wider md:text-[19px] md:leading-[40px] ml-3 md:ml-0 text-[10px] w-[90%] md:w-[40%] my-auto">
-                Passionate and dedicated teacher pursuing a master's in radiology with 5+ years of experience. Specializes in science and math for primary and middle school students, fostering a love for learning and excellence. Join our classroom for an exceptional educational journey!
+                Passionate and dedicated teacher pursuing a master's in radiology with 5+ years of experience in teaching. Specialized in science and math for primary and middle school students, fostering a love for learning and excellence. Join our classroom for an exceptional educational journey!
             </div>
         </div>
     </div>
